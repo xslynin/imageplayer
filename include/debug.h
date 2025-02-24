9130795b29a62bcd077216c08410bb438dd2a738
xslynin <l932297267@163.com>
2025-02-24 16:12:47 +0800

    delete binary file generated by make, add debug to avoid planty of printf and so on, now debug test 1

--- include/debug.h
+++ include/debug.h
@@ -11,8 +11,16 @@ enum LogLevel{
 	FATAL
 };
 
-extern LogLevel this_loglevel;
+//extern LogLevel this_loglevel;
+#define DEBUGMODEL 1
 
-#define DEBUG(level, )
+#ifdef DEBUGMODEL
+#define LOG(level,fmt, ...) do{						\
+						fprintf(stderr,"[" #level "] %s: %s--" fmt "\n" , __func__, __LINE__, ##__VA_ARGS__)				\
+				}while(0)
 
+#else
+#define LOG(...) 
+
+#endif
 #endif
